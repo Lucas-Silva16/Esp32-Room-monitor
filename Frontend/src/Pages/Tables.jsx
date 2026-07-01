ae0a27772dece5932e4587b92ba5106a15c59115
Lucas Silva <pryor1606@gmail.com>
2026-07-01 23:03:40 +0100

    Dados de 10 horas, fix no css das tabelas

--- Frontend/src/Pages/Tables.jsx
+++ Frontend/src/Pages/Tables.jsx
@@ -37,53 +37,55 @@ function Tables() {
     return (
         <section id="tables">
         <div>
-            <h1 className="text-[25px] font-mono font-bold text-center mb-2 text-white">
+            <h1 className="text-[25px] font-mono font-bold text-center mb-5 text-white">
                 Temperature and Humidity
             </h1>
             <div className="w-full overflow-x-auto">
-                <table className="w-full text-left border-collapse font-mono font-bold">
-                    <thead>
-                        <tr className="border-b border-gray-700 text-white">
-                            <th>#</th>
-                            <th>Temp</th>
-                            <th>Humidity</th>
-                            <th>Time</th>
-                        </tr>
-                    </thead>
-                    <tbody  className="divide-y divide-gray-800">
-                    {TempDados.map((row) => (
-                    <tr key={row.ID}>
-                        <td>{row.ID}</td>
-                        <td>{row.temperatura} C</td>
-                        <td>{row.humidade} %</td>
-                        <td>{row.data_leitura}</td>
+            <table className="w-full text-left divide-x divide-gray-700 font-mono font-bold border border-gray-700">
+                <thead>
+                    <tr className="border-b border-gray-700 text-white">
+                        <th className="px-3 py-2 text-center border-x border-gray-700">ID</th>
+                        <th className="px-3 py-2 text-center border-x border-gray-700">Temp</th>
+                        <th className="px-3 py-2 text-center border-x border-gray-700">Humidity</th>
+                        <th className="px-3 py-2 text-center border-x border-gray-700">Time</th>
                     </tr>
-                    ))}
-                    </tbody>
-                </table>
+                </thead>
+                <tbody className="divide-y divide-gray-800">
+                {TempDados.map((row) => (
+                <tr key={row.ID} className="divide-x divide-gray-700">
+                    <td className="px-3 py-2 text-center border-x border-gray-800">{row.ID}</td>
+                    <td className="px-3 py-2 text-center border-x border-gray-800">{row.temperatura} C</td>
+                    <td className="px-3 py-2 text-center border-x border-gray-800">{row.humidade} %</td>
+                    <td className="px-3 py-2 text-center border-x border-gray-800">{row.data_leitura}</td>
+                </tr>
+                ))}
+                </tbody>
+            </table>
 
-                <h1 className="text-[25px] font-mono font-bold text-center mb-2 text-white"> 
-                </h1>
-                <div className="w-full overflow-x-auto">
-                <table className="w-full text-left border-collapse font-mono font-bold">
-                    <thead>
-                        <tr className="border-b border-gray-700 text-white">
-                            <th>#</th>
-                            <th>Distance</th>
-                            <th>Date and Hour</th>
-                        </tr>
-                    </thead>
-                        <tbody  className="divide-y divide-gray-800">
-                    {passagensDados.map((row) => (
-                    <tr key={row.ID}>
-                        <td>{row.ID}</td>
-                        <td>{row.distancia} cm</td>
-                        <td>{row.data_leitura}</td>
+            <h1 className="text-[25px] font-mono font-bold text-center mb-4 mt-5 text-white">
+                Passagens
+            </h1>
+
+            <div className="w-full overflow-x-auto">
+            <table className="w-full text-left border-collapse font-mono font-bold border border-gray-700">
+                <thead>
+                    <tr className="border-b border-gray-700 text-white">
+                        <th className="px-3 py-2 text-center border-x border-gray-700">#</th>
+                        <th className="px-3 py-2 text-center border-x border-gray-700">Distance</th>
+                        <th className="px-3 py-2 text-center border-x border-gray-700">Date and Hour</th>
                     </tr>
-                    ))}
-                    </tbody>
-                </table>
-                </div>
+                </thead>
+                <tbody className="divide-y divide-gray-800">
+                {passagensDados.map((row) => (
+                <tr key={row.ID} className="divide-x divide-gray-800">
+                    <td className="px-3 py-2 text-center border-x border-gray-800">{row.ID}</td>
+                    <td className="px-3 py-2 text-center border-x border-gray-800">{row.distancia} cm</td>
+                    <td className="px-3 py-2 text-center border-x border-gray-800">{row.data_leitura}</td>
+                </tr>
+                ))}
+                </tbody>
+            </table>
+            </div>
             </div>
         </div>
         </section>
